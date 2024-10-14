abf4415e00a7d6e25d0f7719706ac2cd0d7b4383
hlmg <devhlmg@gmail.com>
2024-10-14 18:36:34 +0900

    Initial commit

---
+++ src/main/java/hlmg/practicaltesting/spring/PracticalTestingApplication.java
@@ -0,0 +1,13 @@
+package hlmg.practicaltesting.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalTestingApplication.class, args);
+    }
+
+}
