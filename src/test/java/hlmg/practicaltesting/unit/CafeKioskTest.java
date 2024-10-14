1556995bdb5fec16685ac364934a6c91b7a00d4d
hlmg <devhlmg@gmail.com>
2024-10-14 19:14:39 +0900

    경계값 테스트 추가

--- src/test/java/hlmg/practicaltesting/unit/CafeKioskTest.java
+++ src/test/java/hlmg/practicaltesting/unit/CafeKioskTest.java
@@ -2,20 +2,45 @@ package hlmg.practicaltesting.unit;
 
 import hlmg.practicaltesting.unit.beverage.Americano;
 import hlmg.practicaltesting.unit.beverage.Latte;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CafeKioskTest {
 
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
+
         cafeKiosk.add(new Americano());
 
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
     }
 
     @Test
