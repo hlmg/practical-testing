73bb0cd9f82eb7e692eacce205685af2c39b007e
hlmg <devhlmg@gmail.com>
2024-10-22 20:13:03 +0900

    상품 생성 기능 구현

--- src/main/java/hlmg/practicaltesting/spring/api/controller/product/ProductController.java
+++ src/main/java/hlmg/practicaltesting/spring/api/controller/product/ProductController.java
@@ -1,16 +1,24 @@
 package hlmg.practicaltesting.spring.api.controller.product;
 
+import hlmg.practicaltesting.spring.api.controller.product.dto.request.ProductCreateRequest;
 import hlmg.practicaltesting.spring.api.service.product.ProductService;
 import hlmg.practicaltesting.spring.api.service.product.response.ProductResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class ProductController {
     private final ProductService productService;
+
+    @PostMapping("/api/v1/products/new")
+    public void createProduct(@RequestBody ProductCreateRequest request) {
+        productService.createProduct(request);
+    }
 
     @GetMapping("/api/v1/products/celling")
     public List<ProductResponse> getSellingProducts() {
--- src/main/java/hlmg/practicaltesting/spring/api/service/product/ProductService.java
+++ src/main/java/hlmg/practicaltesting/spring/api/service/product/ProductService.java
@@ -1,5 +1,6 @@
 package hlmg.practicaltesting.spring.api.service.product;
 
+import hlmg.practicaltesting.spring.api.controller.product.dto.request.ProductCreateRequest;
 import hlmg.practicaltesting.spring.api.service.product.response.ProductResponse;
 import hlmg.practicaltesting.spring.domain.product.Product;
 import hlmg.practicaltesting.spring.domain.product.ProductRepository;
@@ -8,11 +9,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -20,5 +33,17 @@ public class ProductService {
         return products.stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
     }
 }
